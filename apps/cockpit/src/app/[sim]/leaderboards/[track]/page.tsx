SimRacingAlliance/SRA-Website

Feat/ACC-Leaderboard-Init

apps/cockpit/src/app/[sim]/leaderboards/[track]/page.tsx
@@ -1,0 +1,41 @@
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+import { getSimBySlug } from '@/content/sims';
+import { getAccTrack, getAccTrackLeaderboard, getAccTrackTopTimes } from '@/lib/acc/tracks';
+import { AccTrackHeader } from '@/components/AccTrackHeader';
+import { AccTrackLeaderboard } from '@/components/AccTrackLeaderboard';
+
+export const dynamic = 'force-dynamic';
+
+export default async function AccTrackLeaderboardPage({
+  params,
+}: {
+  params: Promise<{ sim: string; track: string }>;
+}) {
+  const { sim: slug, track: trackKey } = await params;
+  const sim = getSimBySlug(slug);
+  if (!sim || sim.game !== 'ACC') notFound();
+
+  const track = await getAccTrack(trackKey);
+  if (!track) notFound();
+
+  const [leaderboardByCarGroup, topEntries] = await Promise.all([
+    getAccTrackLeaderboard(trackKey),
+    getAccTrackTopTimes(trackKey, 1),
+  ]);
+
+  return (
+    <section className="max-w-[1280px] mx-auto px-7 pt-14 pb-24">
+      <Link
+        href={`/${sim.slug}/leaderboards`}
+        className="inline-block font-mono text-[13px] tracking-[.2em] uppercase text-txt-3 hover:text-gold transition-colors mb-8"
+      >
+        ← All Tracks
+      </Link>
+
+      <AccTrackHeader track={track} fastestLap={topEntries[0] ?? null} />
+
+      <AccTrackLeaderboard leaderboardByCarGroup={leaderboardByCarGroup} />
+    </section>
+  );
+}

arushevich21/SRA-site

apps/cockpit/src/app/[sim]/leaderboards/[track]/page.tsx
@@ -5,6 +5,9 @@ import { findLeaderboardTrack } from '@/lib/leaderboard-tracks';
 import { getHotLapBoard } from '@/lib/acevo-hotlaps';
 import { HotLapBoard } from '@/components/HotLapBoard';
 import { GameLabel } from '@/components/GameLabel';
+import { getAccTrack, getAccTrackLeaderboard, getAccTrackTopTimes } from '@/lib/acc/tracks';
+import { AccTrackHeader } from '@/components/AccTrackHeader';
+import { AccTrackLeaderboard } from '@/components/AccTrackLeaderboard';
 
 export const dynamic = 'force-dynamic';
 
@@ -16,6 +19,35 @@ export default async function TrackLeaderboardPage({
   const { sim: simSlug, track: trackSlugParam } = await params;
   const sim = getSimBySlug(simSlug);
   if (!sim) notFound();
+
+  // ACC has its own data model (Supabase-backed acc_tracks/acc_hotlap_leaderboard,
+  // per-class breakdown, manufacturer logos) — genuinely different from the
+  // schedule-driven, single-list AC Evo path below, so it gets its own branch
+  // rather than being forced into findLeaderboardTrack/getHotLapBoard.
+  if (sim.game === 'ACC') {
+    const track = await getAccTrack(trackSlugParam);
+    if (!track) notFound();
+
+    const [leaderboardByCarGroup, topEntries] = await Promise.all([
+      getAccTrackLeaderboard(trackSlugParam),
+      getAccTrackTopTimes(trackSlugParam, 1),
+    ]);
+
+    return (
+      <section className="max-w-[1280px] mx-auto px-7 pt-14 pb-24">
+        <Link
+          href={`/${sim.slug}/leaderboards`}
+          className="inline-block font-mono text-[13px] tracking-[.2em] uppercase text-txt-3 hover:text-gold transition-colors mb-8"
+        >
+          ← All Tracks
+        </Link>
+
+        <AccTrackHeader track={track} fastestLap={topEntries[0] ?? null} />
+
+        <AccTrackLeaderboard leaderboardByCarGroup={leaderboardByCarGroup} />
+      </section>
+    );
+  }
 
   const track = findLeaderboardTrack(sim.game, trackSlugParam);
   if (!track) notFound();